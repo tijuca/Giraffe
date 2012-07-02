f96583395152c1e8a3f29afe3bea03f1920e1539
john <john@zarafa.com>
2012-07-02 13:45:57 +0200

    Committing svn revision 35574: Merged [34933] [34944] [35058] : Fixes ZCP-10135

--- common/ustringutil/utfutil.cpp
+++ common/ustringutil/utfutil.cpp
@@ -54,6 +54,7 @@
 #include "utf32iter.h"
 
 #include <unicode/ustring.h>
+#include <charset/convert.h>
 
 #if (U_ICU_VERSION_MAJOR_NUM < 4) || ((U_ICU_VERSION_MAJOR_NUM == 4) && (U_ICU_VERSION_MINOR_NUM < 2))
 #if (U_ICU_VERSION_MAJOR_NUM > 3) || ((U_ICU_VERSION_MAJOR_NUM == 3) && (U_ICU_VERSION_MINOR_NUM >= 6))
@@ -118,7 +119,7 @@ UnicodeString UTF8ToUnicode(const char *utf8)
 	const int32_t length = strlen(utf8);
 	int32_t capacity = length > 512 ? length : 512;
 	int32_t index = 0;
-	
+
 	while (index < length) {
 		UChar32 cp;
 		U8_NEXT(utf8, index, length, cp);
@@ -146,3 +147,20 @@ UnicodeString UTF32ToUnicode(const UChar32 *utf32)
 
 #endif	// ICU >= 3.6
 #endif	// ICU >= 4.2
+
+UnicodeString WCHARToUnicode(const wchar_t *sz)
+{
+	return UTF32ToUnicode((const UChar32 *)sz);
+}
+
+UnicodeString StringToUnicode(const char *sz)
+{
+	std::string strUTF16;
+
+	convert_context converter;
+
+	// *tocode, const _From_Type &_from, size_t cbBytes, const char *fromcode
+	strUTF16 = converter.convert_to<std::string>("UTF-16LE", sz, rawsize(sz), "");
+
+	return UnicodeString((UChar *)strUTF16.data(), strUTF16.length() / 2);
+}
