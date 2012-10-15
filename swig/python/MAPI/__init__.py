8e944ce0e23895cd98591cc7510e3d7ff21441d7
john <john@zarafa.com>
2012-10-15 10:44:24 +0200

    Committing svn revision 37662: Merged [37470] [37472] [37473] [37477] : Fixes ZCP-10748

--- swig/python/MAPI/__init__.py
+++ swig/python/MAPI/__init__.py
@@ -1,5 +1,10 @@
+import sys
 from MAPICore import *
 
 MAPIInitialize(None)
 
 unicode = False
+
+mod_struct = sys.modules.get('MAPI.Struct')
+if mod_struct:
+    mod_struct.MAPIError._initialize_errors()
