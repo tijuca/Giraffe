a90d532ff55f3f97f9b50fe6eb1165daba3b136f
john <john@zarafa.com>
2012-12-07 11:37:33 +0100

    Committing svn revision 39185: Merged [38840] [38850] : Fixes ZCP-11080

--- ECtools/zarafa-archiver/main.cpp
+++ ECtools/zarafa-archiver/main.cpp
@@ -154,6 +154,19 @@ void print_mode_error(modes modeSet, modes modeReq, const char *name)
 	print_help(cerr, name);
 }
 
+static std::string args_to_cmdline(int argc, const char * const argv[])
+{
+	if (argc <= 0)
+		return std::string();
+	
+	std::string strCmdLine(argv[0]);
+
+	for (int i = 1; i < argc; ++i)
+		strCmdLine.append(" '").append(shell_escape(argv[i])).append(1, '\'');
+
+	return strCmdLine;
+}
+
 enum cmdOptions {
 	OPT_USER = 129,
 	OPT_ATTACH,
@@ -221,6 +234,7 @@ int main(int argc, char *argv[])
 	unsigned ulAttachFlags = 0;
 	ArchiverPtr ptrArchiver;
 	convert_context converter;
+	const std::string strCmdLine = args_to_cmdline(argc, argv);
 	std::list<configsetting_t> lSettings;
 	
     ULONG ulFlags = 0;
@@ -451,6 +465,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	ptrArchiver->GetLogger(Archiver::LogOnly)->Log(EC_LOGLEVEL_FATAL, "Startup command: %s", strCmdLine.c_str());
 	ptrArchiver->GetLogger(Archiver::LogOnly)->Log(EC_LOGLEVEL_FATAL, "Version: %s", PROJECT_VERSION_ARCHIVER_STR);
 
 	lSettings = ptrArchiver->GetConfig()->GetAllSettings();
