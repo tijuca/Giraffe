68490964d3fdca6fdcbf4d48387b3f1e99588c5d
Guido Günther <agx@sigxcpu.org>
2015-09-15 07:46:32 +0200

    Simple test using python-zarafa
    testing email delivery and fetch

---
+++ debian/tests/test_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+import unittest
+
+import zarafa
+
+auth_user = os.environ['AUTH_USER']
+auth_pass = os.environ['AUTH_PASS']
+
+
+class TestServer(unittest.TestCase):
+    user = auth_user
+
+    def setUp(self):
+
+        self.server = zarafa.Server(auth_user=auth_user,
+                                    auth_pass=auth_pass)
+
+    def testFindItemBySubject(self):
+        user = self.server.user(self.user)
+        inbox = user.store.folder("Inbox")
+        self.assertIn("the towers", [item.subject for item in inbox.items()])
