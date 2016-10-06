4ca7a798ab768a469a16c650c0066594a446414c
Guido Günther <agx@sigxcpu.org>
2016-10-06 15:17:35 +0200

    Unbreak backup test by using an existing user

--- debian/tests/test_backup.py
+++ debian/tests/test_backup.py
@@ -8,24 +8,25 @@ import kopano
 auth_user = os.environ['AUTH_USER']
 auth_pass = os.environ['AUTH_PASS']
 
-def cmd(c):
-    assert os.system(c) == 0
 
-class TestServer(unittest.TestCase):
-    user = auth_user
+class TestBackup(unittest.TestCase):
+    username = auth_user
+
+    def cmd(self, c):
+        self.assertEqual(os.system(c), 0)
 
     def setUp(self):
-        self.user = kopano.user(self.user)
+        self.user = kopano.user(self.username)
         self.backup_folder = self.user.folder('backup', create=True)
         self.backup_folder.empty()
 
     def testBackupRestoreUser(self):
-        # backup to directory 'user1'
-        cmd('rm user1 -rf')
-        cmd('kopano-backup -u user1')
+        # backup to directory self.user
+        self.cmd('rm -rf %s' % self.username)
+        self.cmd('kopano-backup -u %s' % self.username)
 
         # restore to folder 'backup'
-        cmd('kopano-backup --restore user1 --restore-root=backup')
+        self.cmd('kopano-backup --restore %s --restore-root=backup' % self.username)
 
         # check restored inbox
         inbox = self.user.folder('backup/Inbox')
--- debian/tests/test_server.py
+++ debian/tests/test_server.py
@@ -10,14 +10,13 @@ auth_pass = os.environ['AUTH_PASS']
 
 
 class TestServer(unittest.TestCase):
-    user = auth_user
+    username = auth_user
 
     def setUp(self):
-
         self.server = kopano.Server(auth_user=auth_user,
                                     auth_pass=auth_pass)
 
     def testFindItemBySubject(self):
-        user = self.server.user(self.user)
+        user = self.server.user(self.username)
         inbox = user.store.folder("Inbox")
         self.assertIn("the towers", [item.subject for item in inbox])
